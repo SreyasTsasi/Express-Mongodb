aa2ffc09596fa95c24a82d5a62f5cefe4747772c
sreyas t sasi <71540653+SreyasTsasi@users.noreply.github.com>
2023-11-01 13:30:41 +0530

    Update request-handlers.js

--- request-handlers.js
+++ request-handlers.js
@@ -1,58 +1,3 @@
-// import fs from "fs";
-
-// const dataFilePath = "./data.json";
-
-// function readDataFile() {
-//     try {
-//         const data = fs.readFileSync(dataFilePath, "utf-8");
-//         return JSON.parse(data);
-//     } catch (error) {
-//         return [];
-//     }
-// }
-
-// function writeDataFile(data) {
-//     fs.writeFileSync(dataFilePath, JSON.stringify(data));
-// }
-
-// export function getData(req, res) {
-//     const todoList = readDataFile();
-//     res.json(todoList);
-// }
-
-// export function addTodo(req, res) {
-//     const { todo } = req.body;
-//     const todoList = readDataFile();
-//     todoList.push(todo);
-//     writeDataFile(todoList);
-//     res.json({ message: "Todo added successfully" });
-// }
-
-// export function deleteTodo(req, res) {
-//     const { todo } = req.query;
-//     const todoList = readDataFile();
-//     const index = todoList.indexOf(todo);
-//     if (index !== -1) {
-//         todoList.splice(index, 1);
-//         writeDataFile(todoList);
-//         res.json({ message: "Todo deleted successfully" });
-//     } else {
-//         res.status(404).json({ message: "Todo not found" });
-//     }
-// }
-
-// export function editTodo(req, res) {
-//     const oldTodo = req.query.oldTodo;
-//     const newTodo = req.query.newTodo;
-//     const todoList = readDataFile();
-//     const index = todoList.indexOf(oldTodo);
-//     if (index !== -1) {
-//         todoList[index] = newTodo;
-//         writeDataFile(todoList);
-//         res.json({ message: "Todo updated successfully" });
-//     } else {
-//         res.status(404).json({ message: "Todo not found" });
-//     }
 import UserSchema from "./schemas/userschemas.js";
 
 export async function setData(req ,res) {
@@ -75,6 +20,30 @@ try {
 }
 
 }
+export async function updateData(req,res) {
+  try {
+    let {id} = req.query;
+    let data = req.body;
+    let result = await UserSchema.updateOne({_id: id},data);
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+    res.jsom("some error occured");
+    
+  }
+}
+export async function deleteData(req,res) {
+  try {
+    let {id} =req.query;
+    let result=await UserSchema.deleteOne({_id: id})
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+    res.json("some errors occured")
+    
+  }
+}
+
 
 export function users(req ,res) {
   try{
